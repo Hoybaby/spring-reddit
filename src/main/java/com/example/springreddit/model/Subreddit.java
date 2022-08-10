a49b026dac328842319a9ec0937034c98543893a
Hoybaby <mbartek436@gmail.com>
2022-08-10 15:24:26 -0400

    Adding a new class called Subreddit that is very similar to that of the Post.

---
+++ src/main/java/com/example/springreddit/model/Subreddit.java
@@ -0,0 +1,30 @@
+package com.example.springreddit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Subreddit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message="Community name is required")
+    private String name;
+    @NotBlank(message="Description name is required")
+    private String description;
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Post> posts;
+    private Instant createdDate;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+}
