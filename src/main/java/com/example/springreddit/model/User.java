ce6276328901bbb265d2686bb586a7c96a6b13a1
Hoybaby <mbartek436@gmail.com>
2022-08-10 15:46:07 -0400

    Added the getters and setters to the User class that i forgot to add

--- src/main/java/com/example/springreddit/model/User.java
+++ src/main/java/com/example/springreddit/model/User.java
@@ -34,4 +34,51 @@ public class User {
     private Instant created;
     private boolean enabled;
 
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
