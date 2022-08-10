92a5b376b3af44c48e7579550332d9c20b11c043
Hoybaby <mbartek436@gmail.com>
2022-08-10 15:49:30 -0400

    forget to add the getters and setters to the subreddit class as well. added them now in this commit

--- src/main/java/com/example/springreddit/model/Subreddit.java
+++ src/main/java/com/example/springreddit/model/Subreddit.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.Instant;
+import java.util.List;
 
 @Entity
 @Builder
@@ -27,4 +28,52 @@ public class Subreddit {
     private Instant createdDate;
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
